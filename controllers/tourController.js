19aa0b3dd080b735b094c461d80ed82f850f0fe9
Volodya <hohiop09@gmail.com>
2025-10-18 11:25:32 +0300

    feat: add limitation of fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -55,6 +55,15 @@ export const getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // limitation
+
+    if(req.query.fields) {
+      const fields = req.query.split(',').join(' ')
+      query = query.select(fields)
+    }else {
+      query.select('-__v')
+    }
+
     const tours = await query;
 
     res.status(200).json({
