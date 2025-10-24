244d52cf430f5f8458b6403d4afb7b6d973dc74f
Volodya <hohiop09@gmail.com>
2025-10-24 14:41:54 +0300

    refactor: move errorController to separate file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import toursRouter from "./routes/toursRoutes.js";
 import usersRouter from "./routes/usersRoutes.js";
 import AppError from "./utils/AppError.js";
+import errorController from "./controllers/errorController.js";
 
 const app = express();
 
@@ -15,18 +16,9 @@ app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
 app.use((req, res, next) => {
-
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.status = err.status || 'error';
-  err.statusCode = err.statusCode || 500;
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(errorController);
 
 export default app;
