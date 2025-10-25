ee98556822daafb675a4d1a042290eb01f4148e4
Volodya <hohiop09@gmail.com>
2025-10-25 10:05:49 +0300

    refactor:rewrite error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,38 @@
+const SendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+
+  });
+}
+
+const SendErrorProd = (err, res) => {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    // Programming or other unknown error: don't leak error details
+    } else {
+        console.error('ERROR 💥', err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!'
+        });
+    }
+}  
 
 export default (err, req, res, next) => {
 
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    SendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    SendErrorProd(err, res);
+  }
 }
