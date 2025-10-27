f3704e02d38dfb9b67817543f1427770ac9b4cf6
Volodya <hohiop09@gmail.com>
2025-10-27 12:55:13 +0200

    reafactor: error handling

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./config.env" });
 
+proccess.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+});
+
 const DB = process.env.DATABASE.replace(
   "<db_password>",
   process.env.DB_PASSWORD
@@ -14,3 +19,12 @@ mongoose.connect(DB)
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
