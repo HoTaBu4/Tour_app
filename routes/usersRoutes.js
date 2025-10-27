59de29da8ec60bcaad6caf6659b17823ec0feb0e
Volodya <hohiop09@gmail.com>
2025-10-27 13:45:54 +0200

    feat:add user signup

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { getAllUsers, createUser, getUser, deleteUser } from '../controllers/userController.js';
+import { signup } from '../controllers/authController.js';
 
 const router = express.Router();
+
+router.route("/signup")
+  .post(signup);
 
 router.route("/")
   .get(getAllUsers)
