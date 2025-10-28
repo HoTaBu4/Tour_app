992e54a7210f7912e4b0de8067e3e84887459a0c
Volodya <hohiop09@gmail.com>
2025-10-28 13:49:22 +0200

    feat: add login route fix: signup route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,41 @@
 import User from "../models/userModel.js";
 import CatchAsync from "../utils/catchAsync.js";
+import jwt from "jsonwebtoken";
 
 export const signup = CatchAsync(async (req, res) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+
     res.status(201).json({
         status: 'success',
+        token,
         data: {
-            user: newUser
+            user: newUser,
         }
+    });
+});
+
+export const login = CatchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // 1) Check if email and password exist
+    if (!email || !password) {
+        return next(new Error('Please provide email and password!'));
+    }
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email })
+
+    let token = ''
+    res.status(200).json({
+        status: 'success',
+        token
     });
 });
