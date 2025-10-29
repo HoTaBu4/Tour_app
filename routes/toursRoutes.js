7d5f8bed31ec47d016dd18b8482a4b068597129c
vova <hohiop09@gmail.com>
2025-10-29 20:15:34 +0200

    fix: login error feat: add verification on login

--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getAllTours, createTour, getTour, deleteTour, updateTour ,getTourStats, getMonthlyPlan} from '../controllers/tourController.js';
+import { protect } from '../controllers/authController.js';
 
 const router = express.Router()
 
@@ -10,7 +11,7 @@ router.route('/monthly-plan/:year')
   .get(getMonthlyPlan)
 
 router.route("/")
-  .get(getAllTours)
+  .get( protect, getAllTours )
   .post( createTour);
 
 router.route("/:id")
