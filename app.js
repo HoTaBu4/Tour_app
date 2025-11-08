51f15d44bcf72e8517ec728eca85fb823b41cb69
Volodya <hohiop09@gmail.com>
2025-11-08 09:51:59 +0200

    fix: add rate limiting

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ import toursRouter from "./routes/toursRoutes.js";
 import usersRouter from "./routes/usersRoutes.js";
 import AppError from "./utils/AppError.js";
 import errorController from "./controllers/errorController.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+const limiter = rateLimit({
+  max:100,
+  windowMs:60 * 60 * 1000,
+  message:'Too many requests from this IP, please try again in an hour!'
+})
+
+app.use('/api', limiter);
 
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
